02028d053d77be06aa65c136d217789438ff2fde
InDeLee1818 <168147537+InDeLee1818@users.noreply.github.com>
2024-05-16 19:55:22 -0400

    Add files via upload
    system function

---
+++ System/Source.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+
+
+//system function：在已经运行的程序中执行另外一个外部程序命令，如记事本，画图，计算器，测试网络等等
+// notepad - 记事本
+// mspaint - 画图
+// calc - 计算器
+// ping - 测试网络
+// pause 
+
+
+int main()
+{
+	system("notepad");
+
+	system("mspaint");
+
+	system("calc");
+
+	return 0;
+}
