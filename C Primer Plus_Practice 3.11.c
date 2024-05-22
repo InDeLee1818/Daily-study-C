581b9d09845671f6cd74b2411fc3332bd305ae68
InDeLee1818 <168147537+InDeLee1818@users.noreply.github.com>
2024-05-21 23:30:47 -0400

    Add files via upload
    C Primer Plus_Practice 3.11

---
+++ C Primer Plus_Practice 3.11.c
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//一年大约有3。156 × 10 ^ 7秒。编写一个程序，提升用户输入年龄，然后显示显示该年龄对应的秒数。
+
+/*
+int main()
+{
+	int age;
+	double second;
+	printf("Please enter your age\n");
+	scanf("%d", &age);
+	second = age * 365 * 24 * 60 * 60;
+	printf("you lived for %0.0f seconds\n", second);
+
+	return 0;
+
+}
+*/
+
+
+//一英寸相当于2.54厘米。编写一个程序，提示用户输入身高（／英寸），然后以厘米为单位显示身高。
+/*
+int main()
+{
+	float heigh_cm;
+	float inch;
+	float cm = 2.54;
+
+	printf("Please enter your heigh\n");
+
+	scanf("%f", &inch);
+
+	heigh_cm = inch * cm;
+	printf("your height is %0.2f\n", heigh_cm);
+
+	return 0;
+
+
+}
+*/
+
+
+
+
+//编写一个程序，要求提示输入一个ASCII码值（如，66），然后打印输入的字符。
+
+/*
+int main()
+{
+	char Code_ch;
+
+	printf("Please enter a number\n");
+	scanf("%d", &Code_ch);
+
+	printf("the number for %d is %c\n", Code_ch, Code_ch);
+
+
+	return 0;
+}
+*/
+
