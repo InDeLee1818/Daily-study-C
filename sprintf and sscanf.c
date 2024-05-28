a445965b8611fc4b5167b11d18f68be0acbdec2c
InDeLee1818 <168147537+InDeLee1818@users.noreply.github.com>
2024-05-28 18:10:35 -0400

    Add files via upload
    sprintf and sscanf: Format string operation functions

---
+++ sprintf and sscanf.c
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable:4996)
+#include <stdio.h>
+
+
+
+/*
+//sprintf 格式化字符串操作函数
+int main()
+{
+	char arr1[100];
+	sprintf(arr1, "%d/%d/%d", 2024, 5, 28);
+	printf("arr1 = %s\n", arr1);
+
+	return 0;
+}
+*/
+
+
+//sscanf 格式化字符串操作函数
+int main()
+{
+	int a, b, c;
+	sscanf("2024/5/28", "%d/%d/%d", &a, &b, &c);
+	printf("%d %d %d", a, b, c);
+
+
+	return 0;
+}
