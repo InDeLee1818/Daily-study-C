6d682fec3ff3258603f66e68c962757190f1748d
InDeLee1818 <168147537+InDeLee1818@users.noreply.github.com>
2024-05-31 00:16:40 -0400

    Update sscanf_exercise.c

--- sscanf_exercise.c
+++ sscanf_exercise.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-
+/*
 int main()
 {
 	char arr0[20];
@@ -10,17 +10,31 @@ int main()
 	char arr1[20];
 	//%[^#] = 获取除#号外的所有字符
 	//%*[^#] = 跳过获取除#号外的所有字符
-	//%*# = 跳过获取#号
+	//# = 跳过获取#号
 	//%[^@] = 获取除@号外的所有字符
 	
 
 	sscanf("afj13#fjkads@jf53T", "%*[^#]#%[^@]", arr0);		//获取#与@之间的字符。fjkads
-	sscanf("afj13#fjkads@jf53T", "%*[^#]%*c%[^@]", arr1);	//%*c代替#，其结果一样。fjkads
+	sscanf("afj13#fjkads@jf53T", "%*[^#]%*c%[^@]", arr1);		//%*c代替#，其结果一样。fjkads
 	
 	printf("arr0 = %s\n", arr0);
 	printf("arr1 = %s\n", arr1);
 
 
+	return 0;
+}
+*/
+
+int main()
+{
+	char arr2[20];
+	sscanf("&Makemoremoney$$@", "&%[^@]", arr2);			//&:跳过获取&号
+	printf("arr2 = %s\n", arr2);					//Makemoremoney$$
+
+	char arr3[20];
+	sscanf("&Makemoremoney$$$@", "%*c%[^@]", arr3);			//%*c:跳过获取&号
+	printf("arr3 = %s\n", arr3);					//Makemoremoney$$$
+	
 	return 0;
 }
 
